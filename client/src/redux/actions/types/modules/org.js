1f42c08d1d5d1324b450518e661fe9fda3e0548b
Eric Nielsen <ericnielsen33@gmail.com>
2019-01-24 12:52:32 -0700

    addned new type for org redux

--- client/src/redux/actions/types/modules/org.js
+++ client/src/redux/actions/types/modules/org.js
@@ -1,5 +1,6 @@
 export const ORG = '[ORG]';
 export const FETCH_ORG = `${ORG} FETCH`;
+export const SELECT_ORG = `${ORG} SELECT`
 export const SET_ORG = `${ORG} SET`;
 export const CREATE_ORG = `${ORG} CREATE`
 export const UPDATE_ORG = `${ORG} UPDATE`;
